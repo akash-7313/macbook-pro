79d4d90a6ec53bae90a6cc9170890f39daaacf22
akash-7313 <akashsau79@gmail.com>
2021-08-21 02:48:56 +0600

    third

---
+++ mac.js
@@ -0,0 +1,101 @@
+// function memory
+function memory (memoey8gb) {
+    var memoryPrice = document.getElementById('memory-price');
+
+    if (memoey8gb==true){
+        memoryPrice.innerText = 0;
+    }
+    else {
+        memoryPrice.innerText = 180;
+    }
+    total();
+}
+// memory
+document.getElementById('memory-8gb').addEventListener('click',function(){
+    memory(true);
+})
+document.getElementById('memory-16gb').addEventListener('click',function(){
+    memory(false);
+})
+
+// function storage
+function storage (macStorage){
+    var storagePrice = document.getElementById('storage-price');
+
+    if(macStorage=='256gb'){
+        storagePrice.innerText = 0;
+    }
+    else if (macStorage=='512gb'){
+        storagePrice.innerText = 100;
+    }
+    else {
+        storagePrice.innerText = 180;
+    }
+    total();
+}
+// storage
+document.getElementById('storage-256gb').addEventListener('click', function () {
+    storage('256gb');
+})
+document.getElementById('storage-512gb').addEventListener('click', function () {
+    storage('512gb');
+})
+document.getElementById('storage-1tb').addEventListener('click', function () {
+    storage('1tb');
+})
+
+// function delivery
+function delivery (deliveryLate) {
+    var deliveryPrice = document.getElementById('delivery-cost');
+
+    if (deliveryLate==true){
+        deliveryPrice.innerText = 0;
+    }
+    else {
+        deliveryPrice.innerText = 20;
+    }
+    total();
+}
+// delivery
+document.getElementById('free-delivery').addEventListener('click', function () {
+   delivery(true);
+})
+document.getElementById('paid-delivery').addEventListener('click', function () {
+    delivery(false);
+})
+
+// each feature price
+function eachFeaturePrice (price){
+    var eachPrice = document.getElementById(price);
+    var eachPriceAmount = parseFloat(eachPrice.innerText);
+    return eachPriceAmount;
+}
+// total
+function total () {
+    var bestPrice = document.getElementById('best-price');
+    var bestPriceAmount = parseFloat(bestPrice.innerText);
+
+    var total = document.getElementById('total-price');
+    total.innerText = bestPriceAmount + eachFeaturePrice('memory-price') + eachFeaturePrice('storage-price') + eachFeaturePrice('delivery-cost');
+}
+
+// total after promo
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
